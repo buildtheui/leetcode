b2204a556754fc6951c87696318cd3cc3089d345
Fabián Monsalve <fabtics@gmail.com>
2019-01-31 12:40:31 -0500

    Update palindrome-number.js
    Faster palindrome

--- palindrome-number.js
+++ palindrome-number.js
@@ -1,5 +1,5 @@
 /**
- * 332 ms
+ * Runtime: 332 ms
  * @param {number} x
  * @return {boolean}
  */
@@ -8,4 +8,20 @@ var isPalindrome = function(x) {
     prev = prev.toString();
     x = x.toString().split('').reverse().join().replace(/,/g,'');
     return prev === x;
+};
+
+/**
+ * Runtime: 276 ms
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindromeFaster = function(x) {
+    var defaultValue = x, reverse = 0, pop;
+    if( x < 0 ) return false;
+    while (x !== 0) {
+        pop = x % 10;
+        x = parseInt(x / 10);
+        reverse = reverse * 10 + pop;           
+    }
+    return reverse === defaultValue;
 };
